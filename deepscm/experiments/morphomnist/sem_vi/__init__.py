ec980969d5c681239a5f3f2676f62baab06c1ab5
Rajat Rasal <rrr2417@ic.ac.uk>
2021-02-25 22:27:38 +0000

    1) reversing arrow experiments notebooks 2) experiment classes for reversed arrows

--- deepscm/experiments/morphomnist/sem_vi/__init__.py
+++ deepscm/experiments/morphomnist/sem_vi/__init__.py
@@ -2,6 +2,7 @@ from .base_sem_experiment import SVIExperiment
 from .conditional_decoder_sem import ConditionalDecoderVISEM
 from .independent_sem import IndependentVISEM
 from .conditional_sem import ConditionalVISEM
+from .conditional_sem_pgm_reverse import ConditionalReversedVISEM
 
 __all__ = [
     'SVIExperiment',
